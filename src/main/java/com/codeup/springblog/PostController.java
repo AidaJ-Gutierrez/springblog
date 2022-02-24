76217a1475461d72dfd4a910997c9689328a89c5
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-02-24 09:29:43 -0600

    roll dice and partials finished

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,8 +1,11 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -10,20 +13,27 @@ public class PostController {
 
     @GetMapping(path = "/posts")
     @ResponseBody
-    public String postsIndex(){
-        return "Posts index page";
+//    public String postsIndex(){return "Posts index page";}
+    public  String postIndex(Model model){
+        List<Post> allThePosts = new ArrayList<>();
+        allThePosts.add(new Post("This is Another Post!"));
+        model.addAttribute("post", allThePosts);
+        return "post/index";
     }
+
+
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public String usersPost(@PathVariable long id){
-        return "View " + id + "'s posts";
+    public String individualPost(@PathVariable long id){
+        return "View an individual post";
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String createPostForm(){
-        return "View form for creating posts";
+    public String createPostForm(Model model){
+        model.addAttribute("post", new Post());
+        return "post/create";
     }
 
     @PostMapping("/posts/create")
---
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -0,0 +1,36 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+
+    // returns a view:
+    @GetMapping("/roll-dice")
+    public String rollDice(){
+        return "roll-dice";
+    }
+
+
+    //using models to pass data:
+    @GetMapping("/roll-dice/{n}")
+    public String diceGuess(@PathVariable int n, Model model) {
+        Random randomNum = new Random();
+        int random = randomNum.nextInt((6 - 1) + 1) + 1;
+
+        model.addAttribute("diceRoll", random);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isCorrect", random == n);
+
+        return "roll-dice";
+
+    }
+
+
+}
