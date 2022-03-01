55dbdb0b312fae3aa87d4bed5222fa3993e168d2
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-01 12:15:44 -0600

    form model binding finished

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -28,8 +28,6 @@ public class PostController {
     @GetMapping(path = "/posts")
     public  String postIndex(Model model){
         List<Post> allThePosts = postDao.findAll();
-//        allThePosts.add(new Post(1,"This is one Post!", "This is the body"));
-//        allThePosts.add(new Post(2, "This is one more!", "This is one more body"));
         model.addAttribute("allThePosts", allThePosts);
         return "posts/index";
     }
@@ -37,37 +35,38 @@ public class PostController {
     @GetMapping("/posts/{id}")
     public String individualPost(@PathVariable long id, Model model){
        Post post = postDao.getById(id);
+       model.addAttribute("postID", id);
        model.addAttribute("newPost", post);
         return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
+
     public String createPostForm(Model model){
         model.addAttribute("newPost", new Post());
         return "post/create";
     }
 
     @PostMapping("/posts/create")
-    public String postCreate(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body) {
-        User user = userDao.getById(1L);
-        Post post = new Post(title, body, user);
+    public String createPost(@ModelAttribute Post post) {
+        post.setUser(userDao.getById(1L));
         postDao.save(post);
         return "redirect:/posts";
     }
     @GetMapping("/posts/{id}/edit")
     public String showEditForm(@PathVariable long id, Model model) {
         Post posttoEdit = postDao.getById(id);
-        model.addAttribute("postToEdit", posttoEdit);
+        model.addAttribute("postToEdit", posttoEdit.getId());
         return "posts/edit";
     }
 
     @PostMapping("/posts/{id}/edit")
-    public String submitEdit(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body, @PathVariable long id) {
+    public String submitEdit(@PathVariable long id, @ModelAttribute Post post) {
         Post postToEdit = postDao.getById(id);
-        postToEdit.setTitle(title);
-        postToEdit.setBody(body);
+        postToEdit.setTitle(post.getTitle());
+        postToEdit.setBody(post.getBody());
         postDao.save(postToEdit);
+
         return "redirect:/posts";
     }
 
