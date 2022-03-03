47bf97062b79a75870a6eeb8016016baabb59469
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-03 11:56:42 -0600

    Added book example so I can follow the lecture for testing

--- src/main/java/com/codeup/springblog/controllers/BookController.java
+++ src/main/java/com/codeup/springblog/controllers/BookController.java
@@ -1,50 +1,60 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Book;
+import com.codeup.springblog.models.Genre;
+import com.codeup.springblog.repositories.AuthorRepository;
 import com.codeup.springblog.repositories.BookRepository;
+import com.codeup.springblog.repositories.GenreRepository;
 import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BookController {
     private BookRepository bookDao;
-//    private AuthorRepository authorsDao;
-//   private GenreRepository genreDao;
+    private AuthorRepository authorsDao;
+    private GenreRepository genresDao;
+    private final EmailService emailService;
 
-//    private final EmailService emailService;
-
-
-    public BookController(BookRepository bookDao){
-        this.bookDao= bookDao;
+    public BookController(BookRepository bookDao, AuthorRepository authorsDao, GenreRepository genresDao, EmailService emailService) {
+        this.bookDao = bookDao;
+        this.authorsDao = authorsDao;
+        this.genresDao = genresDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/books")
     public String showBooks(Model model) {
         model.addAttribute("allBooks", bookDao.findAll());
-        return "books";
+        return "books/index";
     }
-//
-//    @GetMapping("/books/create")
-//    public String showCreateForm(Model model){
-//        model.addAttribute("book", new Book());
-//        return "books/create"; // create new folder in templates
-//    }
-//    @PostMapping("/books/crreate")
-//    public String createBook(@ModelAttribute Book book){
-//        book.setAuthor(authorsDao.getById(1L));
-//
-//    }
 
+    @GetMapping("/books/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("genres", genresDao.findAll());
+        return "books/create";
+    }
 
-    //for email services
-//
-//    @GetMapping
-//    public String sendMail()(
-//            emailService.prepareAndSend("Testing", "Did this work")
-//            )
+    @PostMapping("/books/create")
+    public String createBook(@ModelAttribute Book book) {
+        book.setAuthor(authorsDao.getById(1L));
+        bookDao.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/send-email")
+    public String sendEmail() {
+        emailService.prepareAndSend("Testing", "Did this work");
+        return "redirect:/";
+    }
 
 }
--- src/main/java/com/codeup/springblog/services/EmailService.java
+++ src/main/java/com/codeup/springblog/services/EmailService.java
@@ -1,6 +1,5 @@
 package com.codeup.springblog.services;
 
-import com.codeup.springblog.models.Book;
 import com.codeup.springblog.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,6 +22,22 @@ public class EmailService {
         msg.setFrom(from);
         msg.setTo(post.getUser().getEmail());
 //        msg.setTo("example@email.com");//hard coded
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public void prepareAndSend(String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo("example@email.com");
         msg.setSubject(subject);
         msg.setText(body);
 
