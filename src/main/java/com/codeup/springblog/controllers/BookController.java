98fab28eae85a356b090f2ab57c83638c0eff961
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-03 13:51:43 -0600

    lecture finished

--- src/main/java/com/codeup/springblog/controllers/BookController.java
+++ src/main/java/com/codeup/springblog/controllers/BookController.java
@@ -8,10 +8,7 @@ import com.codeup.springblog.repositories.GenreRepository;
 import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.ArrayList;
@@ -23,25 +20,41 @@ public class BookController {
     private AuthorRepository authorsDao;
     private GenreRepository genresDao;
     private final EmailService emailService;
-
     public BookController(BookRepository bookDao, AuthorRepository authorsDao, GenreRepository genresDao, EmailService emailService) {
         this.bookDao = bookDao;
         this.authorsDao = authorsDao;
         this.genresDao = genresDao;
         this.emailService = emailService;
     }
-
     @GetMapping("/books")
     public String showBooks(Model model) {
         model.addAttribute("allBooks", bookDao.findAll());
         return "books/index";
     }
-
     @GetMapping("/books/create")
     public String showCreateForm(Model model) {
         model.addAttribute("book", new Book());
         model.addAttribute("genres", genresDao.findAll());
         return "books/create";
+    }
+
+    @GetMapping("/books/{id}")
+    public String showBook(@PathVariable long id, Model model) {
+        model.addAttribute("singleBook",bookDao.getById(id));
+        return "books/show";
+    }
+
+    @GetMapping("/books/{id}/edit")
+    public String showEditBookForm(@PathVariable long id, Model model) {
+        model.addAttribute("bookToEdit", bookDao.getById(id));
+        return "books/edit";
+    }
+
+    @PostMapping("/books/{id}/edit")
+    public String submitBookEdit(@ModelAttribute Book bookToEdit, @PathVariable long id) {
+        bookToEdit.setAuthor(authorsDao.getById(1L));
+        bookDao.save(bookToEdit);
+        return "redirect:/books/" + id;
     }
 
     @PostMapping("/books/create")
@@ -50,11 +63,16 @@ public class BookController {
         bookDao.save(book);
         return "redirect:/books";
     }
-
     @GetMapping("/send-email")
     public String sendEmail() {
         emailService.prepareAndSend("Testing", "Did this work");
         return "redirect:/";
+    }
+
+    @GetMapping("/books/{id}/delete")
+    public String deleteBook(@PathVariable long id) {
+        bookDao.delete(bookDao.getById(id));
+        return "redirect:/books";
     }
 
 }
--- src/main/java/com/codeup/springblog/repositories/BookRepository.java
+++ src/main/java/com/codeup/springblog/repositories/BookRepository.java
@@ -8,11 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // Will return a SINGLE book with the matching title.
-//    Book findBookByTitle();
-//
-//
-//    // Custom queries may require you to use the @Query annotation, this uses HQL syntax.
-//    @Query("from Book b where b.title like %:bookTitle%")
-//    List<Book> getBookByAuthor(@Param("bookTitle") String title);
+
+    Book findBookByTitle(String title);
+
 }
